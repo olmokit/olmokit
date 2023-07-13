f2cea2f9d43b51d044051d5528214e8f99c70557
kuus <kunderikuus@gmail.com>
2023-07-13 21:20:10 +0200

    chore: cleanup former terser/swc attempts

--- packages/cli/cli-laravel/webpack/optimization.ts
+++ packages/cli/cli-laravel/webpack/optimization.ts
@@ -1,61 +1,34 @@
-// import { createRequire } from "node:module";
-import TerserPlugin from "terser-webpack-plugin";
-import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 import type { JsMinifyOptions as SwcOptions } from "@swc/core";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import TerserPlugin from "terser-webpack-plugin";
 import type { CliLaravel } from "../pm.js";
-
-// const require = createRequire(import.meta.url);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const optimization = (config: CliLaravel.Config) => ({
-  // minimize: false, // uncomment to debug builds
+  mangleWasmImports: true,
   minimizer:
     process.env["NODE_ENV"] === "production"
       ? [
           // @see https://webpack.js.org/plugins/terser-webpack-plugin/#swc
-          // extractComments is not yet supported
+          // NOTE: extractComments is not yet supported
           new TerserPlugin<SwcOptions>({
             minify: TerserPlugin.swcMinify,
             terserOptions: {
               compress: {
                 drop_console: process.env.APP_ENV !== "dev",
               },
+              // output: {
+              //   // the regex should match the one in "./helpers-getters.js" to
+              //   // preserve the banner of the current project
+              //   comments: new RegExp(
+              //     `${config.project.title} v${config.project.packageJson.version}`
+              //   ),
+              // },
             },
           }),
-          // new (require("terser-webpack-plugin"))({
-          //   extractComments: true,
-          //   terserOptions: {
-          //     compress: {
-          //       drop_console: process.env.APP_ENV !== "dev",
-          //     },
-          //     output: {
-          //       // the regex should match the one in "./helpers-getters.js" to
-          //       // preserve the banner of the current project
-          //       comments: new RegExp(
-          //         `${config.project.title} v${config.project.packageJson.version}`
-          //       ),
-          //     },
-          //     // mangle: {
-          //     //   properties: {
-          //     //     keep_quoted: true,
-          //     //     regex: /^_/,
-          //     //     reserved: [],
-          //     //   }
-          //     // }
-          //   },
-          // }),
           new CssMinimizerPlugin(),
-          // new (require("css-minimizer-webpack-plugin"))(),
         ]
       : [],
-  mangleWasmImports: true,
-  // // when using HTTP/2 and long term caching
-  // splitChunks: {
-  //   chunks: "all",
-  //   maxInitialRequests: 30,
-  //   maxAsyncRequests: 30,
-  //   maxSize: 100000
-  // },
 });
 
 export default optimization;
