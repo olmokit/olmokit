ec917d03f0b001a6a79be77e0e141681abc69aa9
kuus <kunderikuus@gmail.com>
2023-11-01 09:06:19 +0100

    feat: barba before enter hook, less assumptions

--- packages/core/barba/index.ts
+++ packages/core/barba/index.ts
@@ -1,4 +1,4 @@
-import _barba from "@barba/core";
+import _barba, { type ITransitionData, type IViewData } from "@barba/core";
 import { $ } from "@olmokit/dom/$";
 import { setDataAttr } from "@olmokit/dom/setDataAttr";
 import { scrollTo } from "../scroll/scrollTo";
@@ -6,7 +6,31 @@ import { scrollTo } from "../scroll/scrollTo";
 // import barbaRouter from "@barba/router";
 export { barbaRoute } from "./route";
 
-export const barba = _barba;
+const barbaOlmo = {
+  autoSmoothScroll: () => {
+    barba.hooks.beforeEnter((data) => {
+      // `data.trigger` is one of HTMLElment | "barba" | "back" | "forward",
+      // scroll only to top when transitioning to a new route by actually clicking
+      // a link, otherwise just keep the scroll position
+      if (typeof data?.trigger !== "string") {
+        scrollTo(0);
+      }
+      barbaOlmo.setNamespace(data);
+    });
+  },
+  /**
+   * Assign a data attribute with the next route namespace on the `<html>` element
+   */
+  setNamespace: (data?: ITransitionData | IViewData) => {
+    if (data?.next) setDataAttr($("html"), "route", data.next.namespace);
+  },
+};
+
+export const barba = _barba as typeof _barba & {
+  olmo: typeof barbaOlmo;
+};
+
+barba.olmo = barbaOlmo;
 
 /**
  * Barba before enter global hook
@@ -18,14 +42,5 @@ export const barba = _barba;
  * @see https://github.com/barbajs/barba/issues/159
  */
 barba.hooks.beforeEnter((data) => {
-  // `data.trigger` is one of HTMLElment | "barba" | "back" | "forward",
-  // scroll only to top when transitioning to a new route by actually clicking
-  // a link, otherwise just keep the scroll position
-  if (typeof data?.trigger !== "string") {
-    scrollTo(0);
-  }
-
-  // assign a data attribute with the next route namespace on the `<html>`
-  // element
-  if (data?.next) setDataAttr($("html"), "route", data.next.namespace);
+  barbaOlmo.setNamespace(data);
 });
