0c2c8b195fc282531f48ab009475951bc06d9f5b
fabsfabs <sfefromsfe@gmail.com>
2024-07-22 18:11:17 +0200

    set correct url form deployer and hooks

--- packages/cli/cli-laravel/scripts/ci.ts
+++ packages/cli/cli-laravel/scripts/ci.ts
@@ -322,7 +322,7 @@ const ciShared: CliLaravel.CmdDeploy.Task = async ({ arg }) => {
     const { log } = arg;
     const { sshkeyvar, port, host: hostRaw, folder, password } = arg.ctx.options;
     let address = "";
-    let cmdPrefx = `rm`;
+    let cmdPrefx = `rsync --recursive --verbose`;
     cmdPrefx = `sshpass -p "${password}" ${cmdPrefx}`;
     if (port) {
       address = `-e 'ssh -p ${port}' ${address}`;
